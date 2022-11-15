0a133c8c2637593440f2f434a70020462eeffd76
HugoDlhOC <hugo.delahaye53@outlook.fr>
2022-11-15 10:49:54 +0100

    add calendar component v0.1.6

--- wealth-health/src/pages/Home/index.jsx
+++ wealth-health/src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import FormEmployee from "../../components/FormEmployee";
 import Header from "../../components/Header";
+import {Calendar} from "@hugo.delahaye53/react-datepicker";
 
 /**
  * This component represents the home page with the employees creation form.
@@ -10,6 +11,7 @@ const Home = () => {
     <div>
       <Header itIsHomePage={true} />
       <FormEmployee />
+        <Calendar languageChoice={"fr"} yearMin={2001} yearMax={2120} returnFormat={"dd/MM/yyyy"} classToggle={"calendar"} defaultDate={new Date()}/>
     </div>
   );
 };
